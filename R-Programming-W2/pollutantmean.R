9fdc27b96ff8e20cb9b86584923561a3cde43ae3
Fabian Nieto <fabian.c.nieto@gmail.com>
2017-06-18 10:29:49 -0500

    Pollutatntmean function fixed bug -- Loading first file

--- R-Programming-W2/pollutantmean.R
+++ R-Programming-W2/pollutantmean.R
@@ -9,8 +9,13 @@ pollutantmean <- function(directory, pollutant, id = 1:32) {
         strpath <- paste(directory, "/", strfile, ".csv", sep="")
         if (file.exists(strpath)) {
             filedata <- read.csv(strpath, header=TRUE)
+            if (files == id[1] ) {
+                #Fisrt file to load
+                filematrix <- filedata
+            }
             filematrix <- merge(filematrix,filedata, all=TRUE)
         }
     }
+    #Calculate the value of the pollutant indicating by the parameter pollutant
     mean(filematrix[,c(pollutant)],na.rm=TRUE)
 }
